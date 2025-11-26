d8afaef4152ba92a5fe07176d6f03e0d36f76563
Sebastián Valdivia <se.valdiviae@duocuc.cl>
2025-11-26 18:48:00 -0300

    feat: Implement RolController for role retrieval, add Spring Boot validation starter, and adjust parent version.

--- src/main/java/com/backend/huertohogar/controller/RolController.java
+++ src/main/java/com/backend/huertohogar/controller/RolController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/roles")
-@CrossOrigin(origins = "http://localhost:3306")
+@CrossOrigin(origins = "*")
 public class RolController {
     private final RolService rolService;
 
