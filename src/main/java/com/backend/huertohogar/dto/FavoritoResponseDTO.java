09ef03ef049373624c02e3f558c56814f7eff379
Sebastián Valdivia <se.valdiviae@duocuc.cl>
2025-12-10 20:31:16 -0300

    feat: enhance FavoritoResponseDTO with additional product details

--- src/main/java/com/backend/huertohogar/dto/FavoritoResponseDTO.java
+++ src/main/java/com/backend/huertohogar/dto/FavoritoResponseDTO.java
@@ -7,7 +7,10 @@ public class FavoritoResponseDTO {
     private Integer id;
     private Integer productoId;
     private String nombreProducto;
+    private String categoriaProducto;
     private Integer precioProducto;
+    private Integer stockProducto;
+    private String descripcionProducto;
     private String imagenProducto;
 
 
@@ -15,8 +18,11 @@ public class FavoritoResponseDTO {
         this.id = favorito.getId();
         this.productoId = favorito.getProducto().getId();
         this.nombreProducto = favorito.getProducto().getNombre();
+        this.categoriaProducto = favorito.getProducto().getCategoria();
         this.precioProducto = favorito.getProducto().getPrecio();
-        this.imagenProducto = favorito.getProducto().getImagen();
+        this.stockProducto = favorito.getProducto().getStock();
+        this.descripcionProducto = favorito.getProducto().getDescripcion();
+        this.imagenProducto = favorito.getProducto().getImagenUrl();
     }
 
     public FavoritoResponseDTO() {}
@@ -51,6 +57,30 @@ public class FavoritoResponseDTO {
 
     public void setPrecioProducto(Integer precioProducto) {
         this.precioProducto = precioProducto;
+    }
+
+    public String getCategoriaProducto() {
+        return categoriaProducto;
+    }
+
+    public void setCategoriaProducto(String categoriaProducto) {
+        this.categoriaProducto = categoriaProducto;
+    }
+
+    public Integer getStockProducto() {
+        return stockProducto;
+    }
+
+    public void setStockProducto(Integer stockProducto) {
+        this.stockProducto = stockProducto;
+    }
+
+    public String getDescripcionProducto() {
+        return descripcionProducto;
+    }
+
+    public void setDescripcionProducto(String descripcionProducto) {
+        this.descripcionProducto = descripcionProducto;
     }
 
     public String getImagenProducto() {
